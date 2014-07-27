9fb756a1fda57338f39dd74a7a6e22326b7ede17
Neelabh Gupta <ngupta@twitter.com>
2014-07-26 21:27:04 -0700

    Moved files into new app dir

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Want something that works the first time as well as for updates...
 git submodule init
 git submodule update
-(cd web/jor1k; git checkout experimental-gcc-errors; git pull)
+(cd app/jor1k; git checkout experimental-gcc-errors; git pull)
 
 # Original prebuilt hdgcc.bz2 is from Sebastian Macke (of jor1k). May eventually be replaced by ouw own linux disk image
-rsync images/hdgcc-mod.bz2 web/bin/
+rsync images/hdgcc-mod.bz2 app/bin/
