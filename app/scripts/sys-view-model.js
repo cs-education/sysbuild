85e67302d58ed3d5c1e34e6c4175323795332c4e
Neelabh Gupta <ngupta@twitter.com>
2014-08-19 04:22:02 -0700

    Refactored code to use viewModel instead of DOM manipulation

---
+++ app/scripts/sys-view-model.js
@@ -0,0 +1,62 @@
+/* global ko */
+
+window.SysViewModel = (function () {
+    'use strict';
+    
+    function SysViewModel() {
+        var self = this;
+        var compileStatusToLabelClassMap = {
+            'Waiting': 'default',
+            'Ready': 'success',
+            'Compiling': 'primary',
+            'Cancelled': 'default',
+            'Success': 'success',
+            'Warnings': 'warning',
+            'Failed': 'danger'
+        };
+
+        var vmStateToLabelClassMap = {
+            'Stopped': 'danger',
+            'Booting': 'warning',
+            'Running': 'success',
+            'Paused': 'default'
+        };
+
+        self.gccErrorCount = ko.observable(0);
+        self.gccWarningCount = ko.observable(0);
+        self.gccOptions = ko.observable('-lm -Wall -fmax-errors=10 -Werror -Wextra');
+        self.programArgs = ko.observable('hello world');
+        self.lastGccOutput = ko.observable('');
+
+        self.compileStatus = ko.observable('Waiting');
+        self.compileStatusClass = ko.pureComputed(function () {
+            return 'label label-' + compileStatusToLabelClassMap[self.compileStatus()];
+        });
+        self.compileBtnEnable = ko.pureComputed(function () {
+            return !(self.compileStatus() === 'Waiting' || self.compileStatus() === 'Compiling');
+        });
+
+        self.errorWarningLabel = ko.pureComputed(function () {
+            var errors = self.gccErrorCount(),
+                warnings = self.gccWarningCount(),
+                str = '';
+
+            str += errors ? (errors + ' error' + (errors > 1 ? 's ' : ' ')) : '';
+            str += warnings ? (warnings + ' warning' + (warnings > 1 ? 's' : '')) : '';
+            return str;
+        });
+
+        self.vmState = ko.observable('Stopped');
+        self.vmStateClass = ko.pureComputed(function () {
+            return 'label label-' + vmStateToLabelClassMap[self.vmState()];
+        });
+        self.vmMips = ko.observable(0);
+
+        self.showLastGccOutput = function () {
+            var output = self.lastGccOutput();
+            if (output) { window.alert(output); }
+        };
+    }
+
+    return SysViewModel;
+})();
