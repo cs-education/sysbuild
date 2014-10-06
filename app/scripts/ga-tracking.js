1a01b8dbe41560f5abbbe7d3ece9baed6974e5d2
Neelabh Gupta <gupta.neelabh@gmail.com>
2014-10-05 22:13:11 -0500

    move ga async script loading into head tag

--- app/scripts/ga-tracking.js
+++ app/scripts/ga-tracking.js
@@ -19,12 +19,7 @@ window.Tracker = (function () {
             window['ga-disable-' + neelabhgGaWebPropertyId] = true;
         }
 
-        // Load analytics.js from Google
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
+        // Create the tracker objects
         ga('create', angraveGaWebPropertyId, 'auto', {'name': 'angrave'});
         ga('create', neelabhgGaWebPropertyId, 'auto', {'name': 'neelabhg'});
     }
