3e2e8f927873f18d0aec39d6b193443ec1dccfd3
Neelabh Gupta <gupta.neelabh@gmail.com>
2014-10-05 21:42:23 -0500

    Added tracking functions

--- app/scripts/ga-tracking.js
+++ app/scripts/ga-tracking.js
@@ -1,6 +1,23 @@
-window.Tracker = (function () {
+/* global ga */
 
-    var init = function () {
+// A singleton encapsulating Google Analytics tracking
+window.Tracker = (function () {
+    'use strict';
+    var instance;
+
+    var angraveGaWebPropertyId = 'UA-42515111-2',
+        neelabhgGaWebPropertyId = 'UA-39700861-4';
+
+    var isProduction = function () {
+        return window.location.hostname + window.location.pathname === 'angrave.github.io/sys/';
+    };
+
+    function Tracker() {
+        // Disable tracking if the opt-out cookie exists or this is not a production application
+        if (document.cookie.indexOf('disableTracking=true') > -1 || !isProduction()) {
+            window['ga-disable-' + angraveGaWebPropertyId] = true;
+            window['ga-disable-' + neelabhgGaWebPropertyId] = true;
+        }
 
         // Load analytics.js from Google
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -8,13 +25,63 @@ window.Tracker = (function () {
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-42515111-2', 'auto', {'name': 'angrave'});
-        ga('create', 'UA-39700861-4', 'auto', {'name': 'neelabhg'});
+        ga('create', angraveGaWebPropertyId, 'auto', {'name': 'angrave'});
+        ga('create', neelabhgGaWebPropertyId, 'auto', {'name': 'neelabhg'});
+    }
+
+    Tracker.prototype.isTrackingEnabled = function () {
+        return !(window['ga-disable-' + angraveGaWebPropertyId] && window['ga-disable-' + neelabhgGaWebPropertyId]);
     };
 
-    ga('send', 'pageview');
+    // Opt-out function
+    Tracker.prototype.optout = function () {
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/advanced
+        document.cookie = 'disableTracking' + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+        window['ga-disable-' + angraveGaWebPropertyId] = true;
+        window['ga-disable-' + neelabhgGaWebPropertyId] = true;
+    };
 
+    /**
+     * Track an event. This function is a wrapper to the analytics.js event tracking implementation
+     * See https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+     * for the parameters to this function.
+     *
+     * When calling this function, do not include the command and hit type parameters.
+     * These parameters are automatically set to 'send' and 'event' respectively.
+     */
+    Tracker.prototype.trackEvent = function () {
+        var args = Array.prototype.slice.call(arguments);
+        args.unshift('angrave.send', 'event');
+        ga.apply(null, args);
+        args[0] = 'neelabhg.send';
+        ga.apply(null, args);
+    };
 
+    /**
+     * Track a page view
+     * @param page optional If not set, the page will be set to the current path including the location hash
+     * @param title optional If not set, the analytics.js library will
+     *        set the title value using the document.title browser property
+     */
+    Tracker.prototype.trackPageView = function (page, title) {
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/pages
+        var properties = {
+            'page': page || window.location.pathname + window.location.search + window.location.hash
+        };
+        if (title) {
+            properties['title'] = title;
+        }
+        ga('angrave.send', 'pageview', properties);
+        ga('neelabhg.send', 'pageview', properties);
+    };
 
-
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = new Tracker();
+                instance.constructor = null;
+            }
+            return instance;
+        }
+    };
 })();
