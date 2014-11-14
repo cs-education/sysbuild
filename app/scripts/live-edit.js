49f037cdd163975058fd3e0c2c8d4aa9c6acd73d
Keagan McClelland <kmcclel2@illinois.edu>
2014-11-14 15:19:18 -0600

    removd Anant's changes to the output escaping function

--- app/scripts/live-edit.js
+++ app/scripts/live-edit.js
@@ -58,7 +58,7 @@ window.LiveEdit = (function () {
 
         this.editor.setAnnotations(aceAnnotations);
         // Fix XSS issue gcc output
-        this.viewModel.lastGccOutput(this.escapeHtml(result.gccOutput));
+        this.viewModel.lastGccOutput(result.gccOutput);
         this.viewModel.gccErrorCount(result.stats.error);
         this.viewModel.gccWarningCount(result.stats.warning);
         this.viewModel.gccOptsError(gccOptsErrors.map(function(error) { return error.text; }).join('\n'));
