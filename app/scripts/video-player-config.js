3e8213e063524067bfddbeb803a99b1f50a35c07
Scott Walters <me@scowalt.com>
2015-04-25 10:53:34 -0500

    FINALLY fixed video subtitles

--- app/scripts/video-player-config.js
+++ app/scripts/video-player-config.js
@@ -4,32 +4,37 @@ window.videoPlayerConfig = (function () {
     'use strict';
 
     function configure(player, videoURL, videoName) {
-        var trackLabel = 'English';
-        player.addRemoteTextTrack({
-            kind: 'captions',
-            language: 'en',
-            label: trackLabel,
-            src: 'http://cs-education.github.io/sysassets/transcriptions/' + videoName + '.webvtt'
-        });
-
-        // now I want to activate the track I just added
-        // unfortunately, there's no good way to do that (as of 2015-04-03)
-        // so, let's use a hack!
-        // get the text track options in the DOM of the video player
-        $('div.video-js > div.vjs-control-bar > div.vjs-captions-button > div.vjs-control-content > ' +
-            'div.vjs-menu > ul.vjs-menu-content > li.vjs-menu-item').each(function (index, element) {
-            // find the option for our text track
-            if (element.innerHTML === trackLabel) {
-                // click the caption option in the DOM
-                element.click();
-            }
-        });
-
         player.src([
             { type: 'video/mp4', src: videoURL + '.mp4' },
             { type: 'video/webm', src: videoURL + '.webm' },
             { type: 'video/ogg', src: videoURL + '.ogv' }
         ]);
+
+        // It's very important that you wait for the video player to load before
+        // adding remote text tracks. Source:
+        // http://docs.brightcove.com/en/video-cloud/brightcove-player/guides/adding-captions-to-videos.html
+        player.on('loadstart', function(){
+            var trackLabel = 'English';
+            player.addRemoteTextTrack({
+                kind: 'captions',
+                language: 'en',
+                label: trackLabel,
+                src: 'http://cs-education.github.io/sysassets/transcriptions/' + videoName + '.webvtt'
+            });
+
+            // now I want to activate the track I just added
+            // unfortunately, there's no good way to do that (as of 2015-04-03)
+            // so, let's use a hack!
+            // get the text track options in the DOM of the video player
+            $('div.video-js > div.vjs-control-bar > div.vjs-captions-button > div.vjs-control-content > ' +
+                'div.vjs-menu > ul.vjs-menu-content > li.vjs-menu-item').each(function (index, element) {
+                // find the option for our text track
+                if (element.innerHTML === trackLabel) {
+                    // click the caption option in the DOM
+                    element.click();
+                }
+            });
+        });
     }
 
     return {
