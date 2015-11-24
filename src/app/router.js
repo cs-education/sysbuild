893d210695ea926bbf8290cd449e082c2024225e
Neelabh Gupta <gupta.neelabh@gmail.com>
2015-11-24 08:13:21 +0530

    Update editorconfig and fix some whitespace

--- src/app/router.js
+++ src/app/router.js
@@ -14,14 +14,14 @@ import hasher from 'hasher';
 class Router {
     constructor(config) {
         this.currentRoute = ko.observable({});
-    
+
         // Configure Crossroads route handlers
         ko.utils.arrayForEach(config.routes, (route) => {
             crossroads.addRoute(route.url, (requestParams) => {
                 this.currentRoute(ko.utils.extend(requestParams, route.params));
             });
         });
-    
+
         // Activate Crossroads
         crossroads.normalizeFn = crossroads.NORM_AS_OBJECT;
         hasher.initialized.add(hash => crossroads.parse(hash));
--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -6,7 +6,7 @@ class NavBarViewModel {
         // This viewmodel doesn't do anything except pass through the 'route' parameter to the view.
         // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
         // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
-        this.route = params.route;        
+        this.route = params.route;
     }
 }
 
