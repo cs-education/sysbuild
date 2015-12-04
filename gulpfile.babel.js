610ede7f65a1fa0d2249ab853985c720a720338e
Neelabh Gupta <gupta.neelabh@gmail.com>
2015-12-05 02:36:31 +0530

    Copy jor1k worker, kernel and fs files during build
    The "bower_modules/jor1k/bin/" path is not very pretty, but it is
    hard-coded in sys-runtime.js. A "non-hacky" way of changing this path
    depending on the environment (development, distribution) would be to
    use some sort of configuration file, but including the file and
    updating it to match the environment would require non-trivial changes
    to the build process, and is thus out-of-scope for this transition.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
+import es from 'event-stream';
 import chalk from 'chalk';
 import gulp from 'gulp';
 import clean from 'gulp-clean';
+import uglify from 'gulp-uglify';
+
 import './gulp-tasks/build-js';
 import './gulp-tasks/build-assets';
 import './gulp-tasks/build-styles';
@@ -13,7 +16,16 @@ gulp.task('clean', () => {
         .pipe(clean());
 });
 
-gulp.task('build', ['html', 'js', 'css:dist', 'fonts', 'images', 'extras'], (callback) => {
+// Copies the Jor1k Web Worker script file, the VM disk image, and the filesystem files
+gulp.task('jor1k', () => {
+    const workerJs = gulp.src('./src/bower_modules/jor1k/bin/jor1k-worker-min.js')
+        .pipe(uglify({ preserveComments: 'some' }));
+    const fsFiles = gulp.src('./src/bower_modules/jor1k/bin/{vmlinux.bin.bz2,basefs-compile.json,sysroot/**}');
+    return es.concat(workerJs, fsFiles)
+        .pipe(gulp.dest('./dist/bower_modules/jor1k/bin/'));
+});
+
+gulp.task('build', ['html', 'js', 'css:dist', 'fonts', 'images', 'extras', 'jor1k'], (callback) => {
     callback();
     console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
 });
