aaa8553ac493b82c35c858904fd3561941bfabff
Neelabh Gupta <gupta.neelabh@gmail.com>
2015-12-04 12:37:24 +0530

    Inline CSS imports only during build
    Now, during development, Sass will not inline library CSS files during
    development, but instead replace them with CSS "@import url()"
    statements. There are two main advantages to this:
    
    * The stylesheet compile time during development will be reduced.
    
    * The browser will load "@import"ed CSS at runtime, which means the
    path to library CSS files remains intact, so that relative paths
    inside those files (font urls, etc) will just work. Now font url
    paths only need to be rewritten during distribution builds, which
    again means faster stylesheet builds during development.

--- gulp-tasks/build-styles.js
+++ gulp-tasks/build-styles.js
@@ -8,7 +8,7 @@ import replace from 'gulp-replace';
 import concat from 'gulp-concat';
 import minifyCss from 'gulp-minify-css';
 
-gulp.task('sass', () => {
+function compiledCssStream(options={}) {
     // main.scss imports all other style files
     return gulp.src('src/styles/main.scss')
         .pipe(plumber())
@@ -16,12 +16,38 @@ gulp.task('sass', () => {
         .pipe(sass.sync({
             outputStyle: 'expanded',
             precision: 10,
-            includePaths: ['.']
+            includePaths: ['src'],
+            importer: function (url, prev, done) {
+                var newUrl;
+                if (options.sassImportPathModifier)
+                    newUrl = options.sassImportPathModifier(url, prev);
+                else
+                    newUrl = url;
+                return { file: newUrl };
+            }
         }).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 1 version']}))
         .pipe(sourcemaps.write())
-        .pipe(concat('styles.css')) // rename file
-        .pipe(gulp.dest('src/'))
+        .pipe(concat('styles.css')); // rename file
+}
+
+gulp.task('styles:src', () => {
+    return compiledCssStream()
+        .pipe(gulp.dest('src/'));
+});
+
+gulp.task('styles:dist', () => {
+    return compiledCssStream({
+            sassImportPathModifier: (url, prev) => {
+                // inline CSS imports
+                if (url.slice(-4) === '.css')
+                    url = url.slice(0, -4);
+                return url;
+            }
+        })
+        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
+        .pipe(minifyCss({compatibility: '*'}))
+        .pipe(gulp.dest('./dist/'));
 });
 
 // Copies fonts
@@ -29,13 +55,5 @@ gulp.task('fonts', () => {
     const bootstrapFonts = gulp.src('./src/bower_modules/components-bootstrap/fonts/*', { base: './src/bower_modules/components-bootstrap/' }),
           notific8Fonts = gulp.src('./src/bower_modules/jquery-notific8/dist/fonts/*', { base: './src/bower_modules/jquery-notific8/dist/' });
     return es.concat(bootstrapFonts, notific8Fonts)
-        .pipe(gulp.dest('./dist/'));
-});
-
-// Rewrites relative paths to fonts, copies CSS file
-gulp.task('css', ['sass'], () => {
-    return gulp.src('src/styles.css')
-        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/'))
-        .pipe(minifyCss({compatibility: '*'}))
         .pipe(gulp.dest('./dist/'));
 });
