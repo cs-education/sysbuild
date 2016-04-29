ececde247d262a1db53039dd06a215fb03ab8b7a
prevosis <aadilzbhatti@gmail.com>
2016-04-29 12:25:19 -0500

    Added functionality of including headers across a directory and within subdirectories. Currently working on adding editor pop-up that informs user of included header file.

--- src/components/editor/auto-include.js
+++ src/components/editor/auto-include.js
@@ -38,39 +38,60 @@ class AutoIncluder {
 	}
 
 	addMissingHeaders(params) {
-        var getter = params.editorParams.editorTextGetter();
-        var text = getter().split('\n');
-        var editor = SysGlobalObservables.Editor.aceEditor;
-        var session = editor.session;
-        var length = session.getLength();
-        var includeMap = self.IncludeMap;
-        var currentHeaders = [];
-        var headers = [];
-        for (var row = 0; row <= length; row++) {
-            var tokens = session.getTokens(row);
-            tokens.forEach(function(token) {
-                var syntax = token.value;
-                if (syntax === '#include' || syntax === '#define') {
-                	var current = tokens.map(function(token) {
-                		return token.value;
-                	});
-                	currentHeaders.push(current.join(''));
-                	return;
-                } else if (syntax in includeMap) {
-                	includeMap[syntax].forEach(function(header) {
-                	    if (headers.indexOf(header) == -1 && currentHeaders.indexOf(header) == -1) {
-                	       	headers.push(header);
-                	    }
-                	});
-            	}
-            });
-        }
-        headers.forEach(function(header) {
-            text.unshift(header);
-        });
-        var final = text.join('\n');
-        SysGlobalObservables.Editor.setAceText(final);
+		var browser = SysGlobalObservables.FileBrowser;
+		var originalGetter = SysGlobalObservables.currentFilePath;
+		var originalFile = originalGetter();
+		var updates = {};
+		for (var file in browser.metaDataPathLookUp) {
+			if (file.length === 0 || (file.indexOf(".h") < 0 && file.indexOf(".c") < 0)) continue;
+			var content = browser.fs.readFileSync(file).toString('binary');
+			browser.makeActive(file);
+			browser.editor.setFile(file, file.substring(1), content);
+			var getter = params.editorParams.editorTextGetter();
+	        var text = getter().split('\n');
+	        var editor = SysGlobalObservables.Editor.aceEditor;
+	        var session = editor.session;
+	        var length = session.getLength();
+	        var includeMap = self.IncludeMap;
+	        var currentHeaders = [];
+	        var headers = [];
+	        for (var row = 0; row <= length; row++) {
+	            var tokens = session.getTokens(row);
+	            tokens.forEach(function(token) {
+	                var syntax = token.value;
+	                if (syntax === '#include' || syntax === '#define') {
+	                	var current = tokens.map(function(token) {
+	                		return token.value;
+	                	});
+	                	currentHeaders.push(current.join(''));
+	                	return;
+	                } else if (syntax in includeMap) {
+	                	includeMap[syntax].forEach(function(header) {
+	                	    if (headers.indexOf(header) == -1 && currentHeaders.indexOf(header) == -1) {
+	                	       	headers.push(header);
+	                	       	if (header in updates) {
+	                	       		updates[header].push(file.substring(1));
+	                	       	} else {
+	                	       		updates[header] = [file.substring(1)];
+	                	       	}
+	                	    }
+	                	});
+	            	}
+	            });
+	        }
+	        headers.forEach(function(header) {
+	            text.unshift(header);
+	        });
+	        var final = text.join('\n');
+	        SysGlobalObservables.Editor.setAceText(final);
+			browser.saveActiveFile();
+		}
+		var originalContent = browser.fs.readFileSync(originalFile).toString('binary');
+		browser.makeActive(originalFile);
+		browser.editor.setFile(originalFile, originalFile.substring(1), originalContent);
 	}
 }
+
+// TODO add an editor annotation
 
 export default AutoIncluder;
